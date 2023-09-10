371cc290b1a0943c5e0b5e90a8829ada3799a25f
arlantat <nguyenduongthang3@gmail.com>
2023-09-11 03:16:35 +0930

    developed schema

--- tft.py
+++ tft.py
@@ -1,3 +1,4 @@
+import sqlite3
 import logging
 import requests
 import os
@@ -7,6 +8,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 SERVERS = ['br1','eun1','euw1','jp1','kr','la1','la2','na1','oc1','ph2','ru','sg2','th2','tr1','tw2','vn2']
 REGIONS = ['americas', 'asia', 'europe', 'sea']
+VERSION = 'VERSION 13.17'
 MAPPINGS = {
     SERVERS[0]: REGIONS[0],
     SERVERS[1]: REGIONS[2],
@@ -27,6 +29,22 @@ MAPPINGS = {
     }
 RIOT_API = os.environ.get('RIOT_API')
 
+def match_info(region, match):
+    url = f"https://{region}.api.riotgames.com/tft/match/v1/matches/{match}"
+    headers = {'X-Riot-Token': RIOT_API}
+    res = requests.get(url, headers=headers)
+    if res.status_code != 200:
+        print(res.status_code)
+        return
+    info = res.json()['info']
+    if info['game_version'].startswith(VERSION):
+        con = sqlite3.connect("matches.db")
+        cur = con.cursor()
+        for participant in info['participants']:
+            ...
+
+
+
 def get_puuid(server, summonerName):
     url = f"https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}"
     headers = {'X-Riot-Token': RIOT_API}
@@ -40,6 +58,7 @@ def get_puuid(server, summonerName):
 
 
 def matches(server, summonerName, count=20):
+    '''return list_of_matches and region'''
     puuid, region = get_puuid(server, summonerName)
     url = f"https://{region}.api.riotgames.com/tft/match/v1/matches/by-puuid/{puuid}/ids?count={count}"
     headers = {'X-Riot-Token': RIOT_API}
@@ -47,8 +66,8 @@ def matches(server, summonerName, count=20):
     if res.status_code != 200:
         print(res.status_code)
         return
-    json = res.json()
-    return json
+    list_of_matches = res.json()
+    return list_of_matches, region
 
 def get_league(server, league):
     '''league: master, grandmaster, master'''
