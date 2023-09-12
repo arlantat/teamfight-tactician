9740eda67323478d9b5ed70b37b8c2294c49cfd5
arlantat <nguyenduongthang3@gmail.com>
2023-09-13 02:44:36 +0930

    optimised requests and down time

--- tft.py
+++ tft.py
@@ -7,13 +7,13 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 
-req = 0
 def main():
     con = sqlite3.connect("raw_matches.db")
     cur = con.cursor()
-    # server = 'vn2'
-    ## region = server_to_region(server)
-    # server_to_matches(cur, server, region)
+    server = 'na1'
+    region = server_to_region(server)
+    server_to_matches(cur, server, region)
+    con.commit()
     check_db(cur)
     con.close()
 
@@ -42,21 +42,24 @@ MAPPINGS = {  # this is not 100% true, for now I can only verify na1 and vn2
 RIOT_API = os.environ.get('RIOT_API')
 
 def insert_match(cursor, server, region, match_id):
+    # save unnecessary get requests
+    cursor.execute("SELECT * FROM matches WHERE match_id = ?", (match_id,))
+    if cursor.fetchone() is not None:
+        return
     url = f"https://{region}.api.riotgames.com/tft/match/v1/matches/{match_id}"
     headers = {'X-Riot-Token': RIOT_API}
     res = requests.get(url, headers=headers)
-    global req
-    req += 1
-    checkreq()
     if res.status_code != 200:
         print(res.status_code)
+        # wait and retry if rate limit exceeded
+        if res.status_code == 429:
+            time.sleep(25)
+            insert_match(cursor, server, region, match_id)
         return
     json = res.json()
     info = json['info']
     if info['game_version'].startswith(VERSION) and info['queue_id'] == 1100:
-        cursor.execute("INSERT OR IGNORE INTO matches (match_id) VALUES (?)", (match_id,))
-        if cursor.rowcount == 0:  # duplicate match
-            return
+        cursor.execute("INSERT INTO matches (match_id) VALUES (?)", (match_id,))
         for participant in info['participants']:
             augments = participant['augments']
             while len(augments) < 3:
@@ -90,7 +93,6 @@ def summoner_to_matches(cursor, server, region, summonerName, count=10) -> list:
     res = requests.get(url, headers=headers)
     global req
     req += 1
-    checkreq()
     if res.status_code != 200:
         print(res.status_code)
         return []
@@ -107,17 +109,13 @@ def server_to_matches(cursor, server, region):
     # grandmasters = get_league(cursor, server, region, 'grandmaster')
     # mastercnt = 2000 - len(challengers) - len(grandmasters)
     # masters = get_league(cursor, server, region, 'master', mastercnt)
-    print(req)
     cnt = 0
     for name in challengers:
         list_of_matches = summoner_to_matches(cursor, server, region, name)
-        print('success')
         cnt += 1
-        print(cnt)
+        print(f"currently player {cnt}")
         for match in list_of_matches:
-            print(req)
-            if match is not None:
-                insert_match(cursor, server, region, match)
+            insert_match(cursor, server, region, match)
     # for name in grandmasters:
     #     list_of_matches = summoner_to_matches(cursor, server, region, name)
     #     for match in list_of_matches:
@@ -136,7 +134,6 @@ def get_league(cursor, server, region, league, mastercnt=0) -> list:
     res = requests.get(url, headers=headers)
     global req
     req += 1
-    checkreq()
     if res.status_code != 200:
         print(res.status_code)
         return []
@@ -158,20 +155,11 @@ def name_to_puuid(server, region, summonerName):
     url = f"https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}"
     headers = {'X-Riot-Token': RIOT_API}
     res = requests.get(url, headers=headers)
-    global req
-    req += 1
-    checkreq()
     if res.status_code != 200:
         print(res.status_code)
         return
     json = res.json()
     return json['puuid']
-
-def checkreq():  # rate limit
-    global req
-    if req >= 100:
-        req = 0
-        time.sleep(60)
 
 def check_db(cursor):
     cursor.execute("SELECT COUNT(*) FROM matches")
